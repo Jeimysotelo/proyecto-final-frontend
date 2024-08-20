16da6e8adde839a20f9e5aa5862ca0fa856d5684
Jeimy Sotelo <jeimysotelosanchez.123@gmail.com>
2024-08-19 22:46:43 -0500

    correcion de errores

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import * as CartSelectors from '../../store/cart/cart.selectors';
-import * as CartActions from '../../store/cart/cart.actions';
-import { Product } from '../../models/product.model';
+import * as CartSelectors from '../store/cart/cart.selectors';
+import * as CartActions from '../store/cart/cart.actions';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-cart',
